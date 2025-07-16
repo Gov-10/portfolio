a43eb002c72e9a397f710a52c4b4adb857c5cc2b
Govind <govindsys1008@gmail.com>
2025-07-16 18:09:47 +0530

    added next app

--- port/myport/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('projects/', views.projects, name='projects'),
-    path('contact_us/', views.contact_us, name='contact_us')
-]
--- port/myport/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return render(request, 'home.html')
-def projects(request):
-    return render(request, 'projects.html')
-def contact_us(request):
-    return render(request, 'contact_us.html')
